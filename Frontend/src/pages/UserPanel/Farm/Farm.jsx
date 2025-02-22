1c7c9425bc9d43504888a02da021c01fc4816337
Atharva <aaombase@gmail.com>
2025-02-22 23:54:14 +0530

    Feat:Added a table for farm data on FarmPage

--- Frontend/src/pages/UserPanel/Farm/Farm.jsx
+++ Frontend/src/pages/UserPanel/Farm/Farm.jsx
@@ -1,4 +1,19 @@
-const Farm = () => {
-  return <h1></h1>;
+const Farm = ({ data }) => {
+  return (
+    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+      <td className="px-6 py-4">{data.name}</td>
+      <td className="px-6 py-4">{data.location}</td>
+      <td className="px-6 py-4">{data.soilType}</td>
+      <td className="px-6 py-4">{data.size}</td>
+      <td className="px-6 py-4">
+        <a
+          href="#"
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Edit
+        </a>
+      </td>
+    </tr>
+  );
 };
 export default Farm;
