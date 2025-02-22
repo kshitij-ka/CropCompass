89af132577d95059af7cfcef07487f8e82417b92
Atharva <aaombase@gmail.com>
2025-02-22 23:49:05 +0530

    Added Dynamic routing on the table name

--- Frontend/src/components/Td.jsx
+++ Frontend/src/components/Td.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 const Td = ({ children }) => {
-  console.log(children);
+  const navigate = useNavigate();
   return (
     <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-      <td className="px-6 py-4">{children.name}</td>
+      <td
+        className="px-6 py-4"
+        onClick={() => {
+          console.log("Td", children._id);
+          navigate(`farmpage/${children._id}`);
+        }}
+      >
+        {children.name}
+      </td>
       <td className="px-6 py-4">{children.location}</td>
       <td className="px-6 py-4">{children.soilType}</td>
+      <td className="px-6 py-4">{children.size}</td>
       <td className="px-6 py-4">
         <a
           href="#"
