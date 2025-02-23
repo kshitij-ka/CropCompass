7b2abd29b9af63b049f937b2f0839951652301cd
Atharva <aaombase@gmail.com>
2025-02-23 07:29:30 +0530

    Fix:Replace class with className

--- Frontend/src/pages/Home/Cards.jsx
+++ Frontend/src/pages/Home/Cards.jsx
@@ -1,112 +1,189 @@
-
 import React from "react";
 
-export const HeroSecn = () =>{
-    return(
-        <section className=" py-40 w-full flex justify-center text-gray-100">
-        <div className="flex flex-col-reverse md:flex-row justify-between w-10/12 h-auto">
-          <div className="container mx-auto flex flex-col justify-between h-full w-full">
-            <div className="text-center md:text-start flex flex-col justify-around h-full">
-              <h1 className="text-6xl md:text-6xl md:w-2/3 md:font-extrabold font-bold">
-                Anything and Everything you Need to know About
-              </h1>
-              <p className="text-2xl font-semibold mb-8 ">
-                Your crops and their Health!
-              </p>
-            </div>
-          </div>
-          <div className="w-full md:w-4/5 object-contain flex justify-center items-center">
-            <img src="/images/plant.png" className="w-full h-auto rounded-3xl shadow-xl" alt="plant" />
+export const HeroSecn = () => {
+  return (
+    <section className=" py-40 w-full flex justify-center text-gray-100">
+      <div className="flex flex-col-reverse md:flex-row justify-between w-10/12 h-auto">
+        <div className="container mx-auto flex flex-col justify-between h-full w-full">
+          <div className="text-center md:text-start flex flex-col justify-around h-full">
+            <h1 className="text-6xl md:text-6xl md:w-2/3 md:font-extrabold font-bold">
+              Anything and Everything you Need to know About
+            </h1>
+            <p className="text-2xl font-semibold mb-8 ">
+              Your crops and their Health!
+            </p>
           </div>
         </div>
-      </section>
-    )
-}
+        <div className="w-full md:w-4/5 object-contain flex justify-center items-center">
+          <img
+            src="/images/plant.png"
+            className="w-full h-auto rounded-3xl shadow-xl"
+            alt="plant"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export const CardWithImage = () => {
-    return (
-        <div class="max-w-sm  rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                  <img class="rounded-t-lg" src="https://i.pinimg.com/736x/07/2b/5f/072b5f6a1630d919ceee1a8569683cf7.jpg" alt="plant" />
-              </a>
-              <div class="p-6 backdrop-blur-md  rounded-b-lg">
-                  <a href="#">
-                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">High tech, high yields?</h5>
-                  </a>
-                  <p class="mb-3 font-normal text-white  dark:text-gray-400">The Kenyan farmers deploying AI to increase productivity
-This article is more than 4 months old. AI apps are increasingly popular among small-scale farmers seeking to improve the quality and quantity of their crop.</p>
-                  <a href="https://www.theguardian.com/world/2024/sep/30/high-tech-high-yields-the-kenyan-farmers-deploying-ai-to-increase-productivity" target="_blank" class="inline-flex shadow-md backdrop-blur-md bg-gradient-to-tr from-gray-700/20 to-gray-50/20 items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg hover:backdrop-blur-xl ">
-                      Read more
-                      <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                      </svg>
-                  </a>
-              </div>
-        </div>
-    )
-}
+  return (
+    <div className="max-w-sm  rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          className="rounded-t-lg"
+          src="https://i.pinimg.com/736x/07/2b/5f/072b5f6a1630d919ceee1a8569683cf7.jpg"
+          alt="plant"
+        />
+      </a>
+      <div className="p-6 backdrop-blur-md  rounded-b-lg">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
+            High tech, high yields?
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-white  dark:text-gray-400">
+          The Kenyan farmers deploying AI to increase productivity This article
+          is more than 4 months old. AI apps are increasingly popular among
+          small-scale farmers seeking to improve the quality and quantity of
+          their crop.
+        </p>
+        <a
+          href="https://www.theguardian.com/world/2024/sep/30/high-tech-high-yields-the-kenyan-farmers-deploying-ai-to-increase-productivity"
+          target="_blank"
+          className="inline-flex shadow-md backdrop-blur-md bg-gradient-to-tr from-gray-700/20 to-gray-50/20 items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg hover:backdrop-blur-xl "
+        >
+          Read more
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export const CardOnlyText = (props) => {
-    return(
-        <div>
-              <a href={props.href} target="_blank" class="block max-w-sm p-6 rounded-lg shadow-md backdrop-blur-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-50 dark:text-white"> {props.headingText} </h5>
-              <p class="font-normal text-gray-50 dark:text-gray-400"> {props.bodyText} </p>
-              </a>
-        </div>
-    )
-}
+  return (
+    <div>
+      <a
+        href={props.href}
+        target="_blank"
+        className="block max-w-sm p-6 rounded-lg shadow-md backdrop-blur-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-50 dark:text-white">
+          {" "}
+          {props.headingText}{" "}
+        </h5>
+        <p className="font-normal text-gray-50 dark:text-gray-400">
+          {" "}
+          {props.bodyText}{" "}
+        </p>
+      </a>
+    </div>
+  );
+};
 
 export const CardWithButton = () => {
-    return(
-        <div class="max-w-sm p-6 backdrop-blur-md rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <a href="https://www.reuters.com/sustainability/land-use-biodiversity/comment-how-empowering-smallholder-farmers-with-ai-tools-can-bolster-global-food-2025-01-10/" target="_blank">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-50 dark:text-white">Empowering smallholder farmers with AI tools can bolster global food security</h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-50 dark:text-gray-400"> AI-powered weather forecasts help rural Indian farmers make informed planting decisions, reducing debt and boosting savings.</p>
-                <a href="https://www.reuters.com/sustainability/land-use-biodiversity/comment-how-empowering-smallholder-farmers-with-ai-tools-can-bolster-global-food-2025-01-10/" target="_blank" class="inline-flex shadow-md backdrop-blur-md bg-gradient-to-tr from-gray-700/20 to-gray-50/20 items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg hover:backdrop-blur-xl ">
-                      Read more
-                      <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                      </svg>
-                  </a>
-            </div>
-    )
-}
+  return (
+    <div className="max-w-sm p-6 backdrop-blur-md rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a
+        href="https://www.reuters.com/sustainability/land-use-biodiversity/comment-how-empowering-smallholder-farmers-with-ai-tools-can-bolster-global-food-2025-01-10/"
+        target="_blank"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-50 dark:text-white">
+          Empowering smallholder farmers with AI tools can bolster global food
+          security
+        </h5>
+      </a>
+      <p className="mb-3 font-normal text-gray-50 dark:text-gray-400">
+        {" "}
+        AI-powered weather forecasts help rural Indian farmers make informed
+        planting decisions, reducing debt and boosting savings.
+      </p>
+      <a
+        href="https://www.reuters.com/sustainability/land-use-biodiversity/comment-how-empowering-smallholder-farmers-with-ai-tools-can-bolster-global-food-2025-01-10/"
+        target="_blank"
+        className="inline-flex shadow-md backdrop-blur-md bg-gradient-to-tr from-gray-700/20 to-gray-50/20 items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg hover:backdrop-blur-xl "
+      >
+        Read more
+        <svg
+          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </a>
+    </div>
+  );
+};
 
 export const CardWithOnlyImage = () => {
-    return(
-        <div class="w-full max-w-sm bg-white rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://theprint.in/economy/telangana-is-the-success-story-of-indian-agritech-ai-tools-soil-testing-e-commerce-more/1630359/" target="_blank">
-                <img class=" rounded-lg" src="https://i.pinimg.com/736x/2b/2a/0f/2b2a0f7003bd3e4201573c1189d600de.jpg" alt="product image" />
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="w-full max-w-sm bg-white rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
+      <a
+        href="https://theprint.in/economy/telangana-is-the-success-story-of-indian-agritech-ai-tools-soil-testing-e-commerce-more/1630359/"
+        target="_blank"
+      >
+        <img
+          className=" rounded-lg"
+          src="https://i.pinimg.com/736x/2b/2a/0f/2b2a0f7003bd3e4201573c1189d600de.jpg"
+          alt="product image"
+        />
+      </a>
+    </div>
+  );
+};
 
-const cards = [HeroSecn, CardWithImage, CardOnlyText, CardWithButton, CardWithImage];
+const cards = [
+  HeroSecn,
+  CardWithImage,
+  CardOnlyText,
+  CardWithButton,
+  CardWithImage,
+];
 
 export default cards;
 
-
 export const CardLayout = () => {
-    return(
-        <div>
-            <HeroSecn />
-            <div className=" flex justify-center">
-                <div className=" flex justify-between  py-8 w-5/6 ">     
-                <cardWithImage />
-                <div className="flex flex-col gap-10 justify-between ">
-                    <cardOnlyText />
-                    <cardWithButton />
-                </div>
+  return (
+    <div>
+      <HeroSecn />
+      <div className=" flex justify-center">
+        <div className=" flex justify-between  py-8 w-5/6 ">
+          <cardWithImage />
+          <div className="flex flex-col gap-10 justify-between ">
+            <cardOnlyText />
+            <cardWithButton />
+          </div>
 
-                <div className=" flex flex-col justify-between">
-                    <cardWithOnlyImage />
-                    <cardOnlyText />
-                </div>
-                </div>
-            </div>
+          <div className=" flex flex-col justify-between">
+            <cardWithOnlyImage />
+            <cardOnlyText />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
