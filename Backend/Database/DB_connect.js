d5d1e16d1f0c539b360ce1a373adfae144caf549
Kshitij <kshitijk@tuta.io>
2025-07-18 01:06:42 +0530

    Moved the query parameter into the correct position and added logging for the final URI for database connect.

--- Backend/Database/DB_connect.js
+++ Backend/Database/DB_connect.js
@@ -3,19 +3,23 @@ const catchAsyncErrors = require("../Middlewares/catchAsyncErrors.js");
 
 const DB_connect = catchAsyncErrors(async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${process.env.DATABASE_NAME}`
-    );
+    const dbUri = `${process.env.MONGODB_URI}/${process.env.DATABASE_NAME}?authSource=admin`;
+    const connectionInstance = await mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     if (!connectionInstance) {
       console.log("MongoDB connection failed");
-    }
-    console.log(
-      "MongoDB connected Successfully on server : " +
+    } else {
+      console.log(
+        "MongoDB connected Successfully to:",
         connectionInstance.connection.host
-    );
+      );
+    }
   } catch (error) {
     console.log("MongoDB connection failed due to some error :", error);
   }
 });
+
 module.exports = DB_connect;
